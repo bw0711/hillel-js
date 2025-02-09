3d88ad3a82dcbe93dfce793a36aa626c30d177a5
Ivan Yeremenko <bw0711@gmail.com>
2025-02-09 10:03:02 +0200

    fix document name

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-const time = prompt( 'Please, insert number of hours' );
-const secondsInAnHour = time * 60 * 60;
-alert( `There are ${secondsInAnHour} second in ${time} hours`);
+// const user = {
+//     name: 'Ivan',
+//     age: 32
+// };
+
+// console.log(user);
+
+// user.name = 'navI';
+
+// console.log(user);
+
+// const colors = ['red', 'green', 'blue'];
+// console.log(colors);
+// colors[1] = 'white';
+// console.log(colors);
+
+// colors.push('black');
+// console.log(colors);
+
+// colors.reverse();
+// console.log(colors);
+
+// // colors.push[0]('yellow');
+// // console.log(colors);
+
+// console.log(null === 0);
+// console.log(null == 0);
+// console.log(null === undefined);
+
+// console.log(1 == '1');
+// console.log(1 === '1');
+
+// const variable = undefined;
+// console.log(typeof variable);
+
+let myName = 'Jack';
+myName += 'Brown';
+console.log(myName);
