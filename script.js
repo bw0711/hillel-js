c66fe21734bc24f2e789b6bf9a2d8d5f33b95d86
Ivan Yeremenko <bw0711@gmail.com>
2025-02-10 21:39:17 +0200

    fix for task 1

--- script.js
+++ script.js
@@ -5,17 +5,9 @@ console.log("boolean: typeof =>", typeof true);
 console.log("number: typeof =>", typeof 42);
 console.log("bigint: typeof =>", typeof 42n);
 console.log("string: typeof =>", typeof "Hello");
-console.log("symbol: typeof =>", typeof Symbol("symbol"));
 console.log("object: typeof =>", typeof {});
 console.log("array: typeof =>", typeof []);
 console.log("function: typeof =>", typeof function() {});
-console.log("date: typeof =>", typeof new Date());
-console.log("regexp: typeof =>", typeof /regex/);
-console.log("map: typeof =>", typeof new Map());
-console.log("set: typeof =>", typeof new Set());
-console.log("weakmap: typeof =>", typeof new WeakMap());
-console.log("weakset: typeof =>", typeof new WeakSet());
-console.log("error: typeof =>", typeof new Error("error"));
 
 // ДЗ 3.2. Числа та рядки
 const str1 = prompt("Введіть перший рядок:");
