ae0a39674aac4a7561969f285018d4c9c9785a13
Ivan Yeremenko <bw0711@gmail.com>
2025-02-13 22:00:24 +0200

    homework and classwork

---
+++ script.js
@@ -0,0 +1,19 @@
+/* ДЗ 4.1. Робота з prompt
+
+За допомогою prompt запитати “ім'я користувача”.
+За допомогою alert вивести "Hello, John! How are you?" , де “John” це те, що ввів користувач
+*/
+
+// const userName = prompt('Please, enter Your name:');
+// alert(`Hello, ${userName}! How are you?`);
+
+/* Дано тризначне число, яке надае користувач, потрибно визначити:
+
+Чи правда, що всі цифри однакові?
+Чи є серед цифр цифри однакові?
+*/
+
+// const userInputValue = prompt('Please, enter 3-digit number:');
+// const separatedInputValue = userInputValue.split('');
+// console.log(separatedInputValue);
+// const areAllDigitsEqual = 
