15b5f025e764e84e4a4ea4badb2bd8c7a0029a30
Ivan Yeremenko <bw0711@gmail.com>
2025-02-14 15:34:29 +0200

    comments fix

--- src/classwork/sixthTask.js
+++ src/classwork/sixthTask.js
@@ -8,6 +8,14 @@ const sum = firstDigit + secondNumber + thirdNumber;
 
 sum % 2 === 0 ? alert ('The numbers are even') : alert('The numbers are odd');
 sum % 5 === 0 ? alert ('The numbers are multiple of 5') : alert('The numbers are multiple of 5');
-firstDigit * secondNumber * thirdNumber > 100 ? alert(true) : alert(false);
-firstDigit === secondNumber && secondNumber === thirdNumber ? alert(true) : alert(false);
-firstDigit === secondNumber || secondNumber === thirdNumber ? alert(true) : alert(false);
+firstDigit * secondNumber * thirdNumber > 100 
+    ? alert("The product of the numbers is greater than 100") 
+    : alert("The product of the numbers is 100 or less");
+
+firstDigit === secondNumber && secondNumber === thirdNumber 
+    ? alert("All three numbers are equal") 
+    : alert("Not all numbers are the same");
+
+firstDigit === secondNumber || secondNumber === thirdNumber 
+    ? alert("At least two numbers are equal") 
+    : alert("All numbers are different");
