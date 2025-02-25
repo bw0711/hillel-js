e4fa98b828bde4ae1deae276a8c00ae8b8211ece
Ivan Yeremenko <bw0711@gmail.com>
2025-02-25 12:43:12 +0200

    const name changed

--- task3.js
+++ task3.js
@@ -1,11 +1,11 @@
 const LIMIT = 10;
-const minUserInput = 100;
+const MIN_INPUT_VALUE = 100;
 
 const checkUserInput = () => {
     for (let i = 0; i < LIMIT; i++) {
         const userInput = parseInt(prompt('Please, enter a digit that is greater than 100:'));
 
-        if (!isNaN(userInput) && userInput > minUserInput) {
+        if (!isNaN(userInput) && userInput > MIN_INPUT_VALUE) {
             return console.log(`Your digit is ${userInput}`);
         } else {
             alert('Please, enter a number greater than 100!');
