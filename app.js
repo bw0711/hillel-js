d393c9e4ae69481e95b0593571456b182fd830a5
Ivan Yeremenko <bw0711@gmail.com>
2025-03-09 14:38:55 +0200

    added tasks 3 and 4

--- app.js
+++ app.js
@@ -435,3 +435,23 @@ const userOrdersPrice = (listOfOrders) =>
         }, {});
 
 console.log(userOrdersPrice(orders));
+
+const uniqueItems = (orders) => new Set([].concat(...orders.map(order => order.items.map(item => item.name))));
+
+console.log(uniqueItems(orders));
+
+const biggestSpender = (orders) => {
+  const userSpending = orders.reduce((acc, order) => {
+    const total = order.items.reduce((sum, item) => sum + item.price, 0);
+    acc[order.user] = (acc[order.user] || 0) + total;
+    return acc;
+  }, {});
+
+  const [topUser, maxSpent] = Object.entries(userSpending).reduce((max, entry) =>
+    entry[1] > max[1] ? entry : max
+  );
+
+  return `${topUser} spent more than everyone else: $${maxSpent}`;
+};
+
+console.log(biggestSpender(orders));
